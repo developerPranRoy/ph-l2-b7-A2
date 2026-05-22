eecd32ef5e4b7ac1292653a85930249e2e5a5ab8
Pran Roy <pranroy310@gmail.com>
2026-05-22 10:51:10 +0600

    facing issue to deploy

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,11 @@
-import app from './app';
+import app from "./app";
 import express from "express";
-import config from "./config/config";
-import { initDB } from './db';
+import { initDB } from "./db";
+
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
 
+initDB().catch(console.error);
 
-const main = () => {
-    initDB()
-    app.listen(config.port, () => {
-        console.log(`App listening on port ${config.port}`)
-    })
-}
-main()
+export default app;
